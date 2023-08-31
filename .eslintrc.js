f85ef5406e93303627780f018da38864c85a787f
jou2512 <64369196+jou2512@users.noreply.github.com>
2023-08-31 12:18:05 +0200

    first stage

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@voiceflow/eslint-config', '@voiceflow/eslint-config/frontend', '@voiceflow/eslint-config/typescript'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'off',
+  },
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
+};
